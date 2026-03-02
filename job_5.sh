df8af3fc25ab1edc95bfb9b4a0db1f1cbc7a6cec
pcrooijendijk <pien.rooijendijk@yahoo.com>
2026-03-02 12:20:55 +0100

    Added job files

---
+++ job_5.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --account=cseduproject
+#SBATCH --partition=csedu-prio,csedu
+#SBATCH --qos=csedu-small
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=15G
+#SBATCH --gres=gpu:1
+#SBATCH --time=8:00:00
+#SBATCH --output=FL-%j.out
+#SBATCH --error=FL-%j.err
+
+# Navigate to the project directory
+cd /vol/csedu-nobackup/project/prooijendijk/MSR2
+
+# Commands to run your program go here, e.g.:
+ID=5
+python augment_mentions.py --repos-json html_lists/html_list_${ID}.json --github-token ${ID}
